d7aa892d1f9dd3c4262e0487ea83abf94a6769d8
nguyenducmy393 <nguyenducmy393@gmail.com>
2022-03-05 10:16:47 +0700

    update lesson1

--- json-demo.js
+++ json-demo.js
@@ -1,7 +1,7 @@
 /* Learn basic Node JS */
 // 01. Parse String to JSON
 
-var student_string = '{"name": "Nguyen Duc My","age": 26}';
+var student_string = '{"name": "Nguyen Duc My","age": 30}';
 
 var student_obj = JSON.parse(student_string);
 
@@ -15,7 +15,7 @@ var student_string = '{"name": "Nguyen Duc My","age": 26}';
 
 var student_obj = JSON.parse(student_string);
 
-console.log("--OBJECT--")
+console.log("--OBJECT--");
 console.log(`Name: ${student_obj.name}`);
 console.log(`Age: ${student_obj.age}`);
 
@@ -24,8 +24,18 @@ console.log(JSON.stringify(student_obj));
 
 // 03. Save JSON to file
 var student_obj = {
-    name: "Nguyen Van Cuong",
-    age: "26"
+    name: "Nguyen Duc My",
+    age: "30",
 };
 
 var student_string = JSON.stringify(student_obj);
+
+// Save file
+var storage = require("node-persist");
+storage.initSync();
+storage.setItemSync("student", student_string);
+
+// READ file
+
+var content = storage.getItem("student");
+console.log(`content: ${content}`);
