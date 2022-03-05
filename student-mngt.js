1a918c3008913845b26c323c54af88d16abdaea8
nguyenducmy393 <nguyenducmy393@gmail.com>
2022-03-05 15:36:52 +0700

    student mngt commit

--- student-mngt.js
+++ student-mngt.js
@@ -5,14 +5,72 @@ storage.initSync({
 });
 
 // List of all student
-function allStudent() {}
+function getAllStudents() {
+    var students = storage.getItemSync("students");
 
-function getStudent() {}
+    if (typeof students === "undefined") {
+        return [];
+    }
 
-function addStudent() {}
+    return students;
+}
 
-function updateStudent() {}
+// Add one student to list
+function addStudent(id, fullName) {
+    var students = getAllStudents();
+    students.push({
+        id: id,
+        fullname: fullName,
+    });
 
-function removeStudent() {}
+    storage.setItemSync("students", students);
+}
 
-function getStudentDetail() {}
+// Update student
+function updateStudent(studentID, studentName) {
+    var students = getAllStudents();
+    for (var i = 0; i < students.length; i++) {
+        if (students[i].id === studentID) {
+            students[i].fullname = studentName;
+        }
+    }
+
+    storage.setItemSync("students", students);
+}
+
+function removeStudent(studentID) {
+    var students = getAllStudents();
+    for (var i = 0; i < students.length; i++) {
+        if (students[i].id === studentID) {
+            students.splice(i, 1);
+        }
+    }
+
+    storage.setItemSync("students", students);
+}
+
+function getStudentDetail(studentID) {}
+
+// show all list student by console.log
+function showAllStudent() {
+    var students = getAllStudents();
+    students.forEach(function(student) {
+        console.log("Student: " + student.fullname + " (" + student.id + ")");
+    });
+}
+
+function clearAllStudent() {
+    storage.removeItem("students");
+}
+
+addStudent(1, "Cuong");
+addStudent(2, "Kinh");
+addStudent(3, "Chinh");
+addStudent(4, "Quyen");
+showAllStudent();
+updateStudent(4, "Quyen Update");
+removeStudent(2);
+
+// clearAllStudent();
+
+showAllStudent();
