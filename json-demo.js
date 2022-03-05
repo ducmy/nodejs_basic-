5ae5c23cb95e144775b38de441cb61b44309ecfd
nguyenducmy393 <nguyenducmy393@gmail.com>
2022-03-05 09:50:36 +0700

    add git ignore

--- json-demo.js
+++ json-demo.js
@@ -9,4 +9,23 @@ console.log(`Name: ${student_obj.name}`);
 
 console.log(`Age: ${student_obj.age}`);
 
-// 02. Parse JSON to String
+// 02. Convert JSON to String
+
+var student_string = '{"name": "Nguyen Duc My","age": 26}';
+
+var student_obj = JSON.parse(student_string);
+
+console.log("--OBJECT--")
+console.log(`Name: ${student_obj.name}`);
+console.log(`Age: ${student_obj.age}`);
+
+console.log("--STRING--");
+console.log(JSON.stringify(student_obj));
+
+// 03. Save JSON to file
+var student_obj = {
+    name: "Nguyen Van Cuong",
+    age: "26"
+};
+
+var student_string = JSON.stringify(student_obj);
